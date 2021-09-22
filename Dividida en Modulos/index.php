fa601188f67c1455f509a0e4d739787e9771bb1a
JuanBalza <83681810+JuanBalza@users.noreply.github.com>
2021-09-22 15:17:44 -0300

    Dividida en Modulos

---
+++ Dividida en Modulos/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>shop - home</title>
+        <link rel="icon" type="image/x-icon" href="img/favicon.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+<?php include('navbar.php') ?>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container d-flex h-100 align-items-center">
+              <div class="col-6 mx-auto">
+                <div class="text-left">
+                    <h1 class="my-0">Comprá online desde tu casa.</h1>
+                    <h2 class="mt-3 mb-5">¡Nosotros te lo llevamos!</h2>
+                </div>
+              </div>
+              <div class="col-6">
+                <img src="img/hero.png"/>
+              </div>
+
+            </div>
+        </header>
+        <!-- Productos-->
+          <?php include('productos.php') ?>
+        <!-- Contacto-->
+          <?php include('contacto.php') ?>
+
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
